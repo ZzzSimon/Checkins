d1151a3ec3e797160db309675d501b27b03e8864
Zzz <2234587454@qq.com>
2016-10-26 15:10:59 +0800

    Default app

---
+++ app/src/main/java/com/b140414/njupt/checkins/MainActivity.java
@@ -0,0 +1,13 @@
+package com.b140414.njupt.checkins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
